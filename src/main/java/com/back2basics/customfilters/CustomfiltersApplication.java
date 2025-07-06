f13ef0875408cd80642a0dfb033a58571abd1b3f
swlee <swo98@ajou.ac.kr>
2025-07-06 16:30:40 +0900

    init: initial setting

---
+++ src/main/java/com/back2basics/customfilters/CustomfiltersApplication.java
@@ -0,0 +1,13 @@
+package com.back2basics.customfilters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomfiltersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomfiltersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/back2basics/customfilters/CustomfiltersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back2basics.customfilters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomfiltersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
