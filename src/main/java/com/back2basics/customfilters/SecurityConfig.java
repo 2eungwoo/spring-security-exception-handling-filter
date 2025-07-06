76d85377fffa24e8fd79f39f1ba45897f9954a40
swlee <swo98@ajou.ac.kr>
2025-07-06 16:40:54 +0900

    fix: security config fixed

--- src/main/java/com/back2basics/customfilters/SecurityConfig.java
+++ src/main/java/com/back2basics/customfilters/SecurityConfig.java
@@ -1,5 +1,9 @@
 package com.back2basics.customfilters;
 
+import com.back2basics.customfilters.filters.CustomAccessDeniedHandler;
+import com.back2basics.customfilters.filters.CustomAuthFailureHandler;
+import com.back2basics.customfilters.filters.CustomAuthenticationEntryPoint;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -16,24 +20,39 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final CustomAuthFailureHandler failureHandler;
+    private final CustomAuthenticationEntryPoint entryPoint;
+    private final CustomAccessDeniedHandler accessDeniedHandler;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
             .csrf(AbstractHttpConfigurer::disable)
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/api/public/**").permitAll()
+                .requestMatchers("/public/**").permitAll()
                 .anyRequest().authenticated()
             )
-            .formLogin(Customizer.withDefaults());
+            .formLogin(form -> form
+                .loginProcessingUrl("/login")
+                .failureHandler(failureHandler)
+                .permitAll()
+            )
+            .exceptionHandling(ex -> ex
+                .authenticationEntryPoint(entryPoint)
+                .accessDeniedHandler(accessDeniedHandler)
+            );
+
         return http.build();
     }
 
     @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user = User.withUsername("user")
-            .password(passwordEncoder().encode("password"))
+    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+        UserDetails user = User.builder()
+            .username("test")
+            .password(encoder.encode("1234"))
             .roles("USER")
             .build();
         return new InMemoryUserDetailsManager(user);
